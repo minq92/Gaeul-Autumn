37beaea54527e44ca529d0cda369e2fbb93b4775
kimcalm <calmszone@gmail.com>
2023-10-12 16:26:46 +0900

    add of basename

--- minq/src/App.js
+++ minq/src/App.js
@@ -6,7 +6,7 @@ import ResearchTalks from './components/pages/ResearchTalks';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Publications" element={<Publications />} />
